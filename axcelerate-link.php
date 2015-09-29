dde20daea48a67e3295a9def69f4375bf296121d
loremuelgadrinab <loremuelgadrinab@gmail.com>
2015-09-29 10:07:15 +0800

    new updater

--- axcelerate-link.php
+++ axcelerate-link.php
@@ -11,7 +11,14 @@
 		Desc: add CSS for plugin
 		Auth: Loremuel Gadrinab 
 	*/
-	include_once('updater.php');
+require 'plugin-update-checker/plugin-update-checker.php';
+$className = PucFactory::getLatestClassVersion('PucGitHubChecker');
+$myUpdateChecker = new $className(
+	'https://github.com/loremuelgadrinab/axcelerate-link-test/',
+	__FILE__,
+	'master'
+);
+
 	include_once 'axcelerate-link-dbdata.php';
 
 	add_action( 'wp_enqueue_scripts', 'register_plugin_styles' );
@@ -331,20 +338,5 @@ function myEndSession() {
 
 }
 
-if (is_admin()) { // note the use of is_admin() to double check that this is happening in the admin
-	$config = array(
-		'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
-		'proper_folder_name' => 'axcelerate-link', // this is the name of the folder your plugin lives in
-		'api_url' => 'https://api.github.com/repos/loremuelgadrinab/axcelerate-link-test', // the GitHub API url of your GitHub repo
-		'raw_url' => 'https://raw.github.com/loremuelgadrinab/axcelerate-link-testmaster', // the GitHub raw url of your GitHub repo
-		'github_url' => 'https://github.com/loremuelgadrinab/axcelerate-link-test', // the GitHub url of your GitHub repo
-		'zip_url' => 'https://github.com/loremuelgadrinab/axcelerate-link-test/zipball/master', // the zip url of the GitHub repo
-		'sslverify' => true, // whether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
-		'requires' => '3.0', // which version of WordPress does your plugin require?
-		'tested' => '3.3', // which version of WordPress is your plugin tested up to?
-		'readme' => 'README.md', // which file to use as the readme for the version number
-		'access_token' => '', // Access private repositories by authorizing under Appearance > GitHub Updates when this example plugin is installed
-	);
-	new WP_GitHub_Updater($config);
-}
+
 ?>
